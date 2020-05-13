b89fc4e0760775323bc526125bbc8032050c9482
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-05-13 13:49:59 +0100

    when setting the weatherData state, retrieveWeatherData() wasn't spreading both the current and new data, so was sending a second array, within the array

--- src/components/SearchBar/weatherDataRetrieval/weatherDataRetrieval.test.js
+++ src/components/SearchBar/weatherDataRetrieval/weatherDataRetrieval.test.js
@@ -29,24 +29,105 @@ const setSearchOutput = (val) => {};
 
 test("retrieveWeatherData will set the 'weatherData' and 'searchOutput' state if given a valid year", async () => {
     
-    document.getElementById(yearInput.id).value = "2020";
+    document.getElementById(yearInput.id).value = "2008";
     document.getElementById(trackInput.id).value = "all";
     const setWeatherData = jest.fn();
     const setSearchOutput = jest.fn();
     
-    const settingsJSON = JSON.stringify({
-        "oldest_year_available": "2017",
-        "meteostat_API_key": ""
-    });
-    fetch.mockResponse(settingsJSON);
+    //Mock multiple fetch calls
+    fetch.mockResponses(
+        [
+            JSON.stringify({ //Settings JSON
+                oldest_year_available: "1980",
+                meteostat_API_key: ""
+            }),
+            { status: 200 }
+        ],
+        [
+            //The F1 data for a single race
+            String.raw`{"MRData":{"xmlns":"http:\/\/ergast.com\/mrd\/1.4","series":"f1",
+            "url":"http://ergast.com/api/f1/2008/races.json","limit":"30","offset":"0","total":"18","RaceTable":
+            {"season":"2008","Races":[{"season":"2008","round":"1",
+            "url":"http:\/\/en.wikipedia.org\/wiki\/2008_Australian_Grand_Prix","raceName":"Australian Grand Prix",
+            "Circuit":{"circuitId":"albert_park","url":"http:\/\/en.wikipedia.org\/wiki\/Melbourne_Grand_Prix_Circuit",
+            "circuitName":"Albert Park Grand Prix Circuit","Location":{"lat":"-37.8497","long":"144.968",
+            "locality":"Melbourne","country":"Australia"}},"date":"2008-03-16","time":"04:30:00Z"}]}}}`,
+            { status: 200 }
+        ],
+        [
+            //Station ID response
+            String.raw`{"meta":{"source":"National Oceanic and Atmospheric Administration, Deutscher Wetterdienst"},
+            "data":[{"id":"94868","name":"Melbourne","distance":"2.3"}]}`,
+            {status: 200}
+        ],
+        [
+            //Weather by station and date.
+            String.raw`{"meta":{"source":"National Oceanic and Atmospheric Administration, Deutscher Wetterdienst"},
+            "data":[{"date":"2008-03-16","temperature":null,"temperature_min":19.2,"temperature_max":39.1,
+            "precipitation":null,"snowfall":null,"snowdepth":null,"winddirection":null,"windspeed":null,
+            "peakgust":null,"sunshine":null,"pressure":null}]}`,
+            {status: 200}
+        ]
+    );
+    
+    //A year after the one we're retrieving
+    const currentWeatherData = [
+        { year: '2009',
+        circuitId: 'albert_park',
+        circuitName: 'Albert Park Grand Prix Circuit',
+        raceName: 'Australian Grand Prix',
+        round: '1',
+        locality: 'Melbourne',
+        country: 'Australia',
+        lat: '-37.8497',
+        long: '144.968',
+        raceDate: '2009-03-16',
+        raceTime: '04:30:00Z',
+        stationID: '94868',
+        weather: {  } 
+        }
+    ];
+    
+    const expectedOutputResult = [
+        { year: '2008',
+        circuitId: 'albert_park',
+        circuitName: 'Albert Park Grand Prix Circuit',
+        raceName: 'Australian Grand Prix',
+        round: '1',
+        locality: 'Melbourne',
+        country: 'Australia',
+        lat: '-37.8497',
+        long: '144.968',
+        raceDate: '2008-03-16',
+        raceTime: '04:30:00Z',
+        stationID: '94868',
+        weather:
+         { date: '2008-03-16',
+           temperature: null,
+           temperature_min: 19.2,
+           temperature_max: 39.1,
+           precipitation: null,
+           snowfall: null,
+           snowdepth: null,
+           winddirection: null,
+           windspeed: null,
+           peakgust: null,
+           sunshine: null,
+           pressure: null } 
+        }
+    ];
+    
+    const expectedWeatherDataStateResult = [
+        ...currentWeatherData,
+        ...expectedOutputResult
+    ];
     
     
+    await retrieveWeatherData(yearInput.id, trackInput.id, apiSettings, setApiSettings, currentWeatherData, setWeatherData, setSearchOutput);
     
-    await retrieveWeatherData(yearInput.id, trackInput.id, apiSettings, setApiSettings, {}, setWeatherData, setSearchOutput);
     
-    
-    expect(setWeatherData).toHaveBeenCalled();
-    expect(setSearchOutput).toHaveBeenCalled();
+    expect(setWeatherData).toHaveBeenCalledWith(expect.arrayContaining(expectedWeatherDataStateResult));
+    expect(setSearchOutput).toHaveBeenCalledWith(expect.arrayContaining(expectedOutputResult));
 });
 
 
