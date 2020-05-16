c2eea29223bd0c4b04ec1c0a4f73d4e37db556f1
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-05-16 13:42:09 +0100

    ResultContainer was failing. Corrected to use React.Children

--- src/components/ResultContainer/ResultContainer.js
+++ src/components/ResultContainer/ResultContainer.js
@@ -3,11 +3,9 @@ import './ResultContainer.css';
 
 function ResultContainer(props)
 {
-    const children = (props.hasOwnProperty("children")) ? props.children.map((child) => child) : "";
-    
     return (
         <div className="ResultContainer">
-            {children}
+            {React.Children.map(props.children, (child) => child)}
         </div>
     );
 }
