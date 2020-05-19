201dd7536923e05f45b3662fb9dfbae638f6c88b
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-05-19 17:22:16 +0100

    the APIReferences links were targeted to a new tab, but didn't have a relationship of noopener and noreferrer. This was causing security warnings, so added the relations to the links

--- src/components/APIReferences/APIReferences.js
+++ src/components/APIReferences/APIReferences.js
@@ -7,14 +7,14 @@ function APIReferences()
     return(
         <div className="APIReferences">
             <div className="APIReference">
-                <a href="https://www.meteostat.net" title="meteostat" target="_blank">Meteostat</a>. Meteorological 
+                <a href="https://www.meteostat.net" title="meteostat" target="_blank" rel="noreferrer noopener">Meteostat</a>. Meteorological 
                     data: Copyright &copy; National Oceanic and Atmospheric Administration (NOAA), Deutscher 
                     Wetterdienst (DWD). Learn more about the <a href="https://www.meteostat.net/sources" 
-                    title="meteostat Sources" target="_blank">sources</a>.
+                    title="meteostat Sources" target="_blank" rel="noopener noreferrer">sources</a>.
             </div>
             
             <div className="APIReference">
-                F1 data provided by the <a href="https://ergast.com/mrd/" title="ergast" target="_blank">Ergast Developer API</a>.
+                F1 data provided by the <a href="https://ergast.com/mrd/" title="ergast" target="_blank" rel="noopener noreferrer">Ergast Developer API</a>.
             </div>
         </div>
     );
