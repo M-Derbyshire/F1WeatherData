3d2d9ee97a6f72eb57c657d7da288b8d877817e3
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-21 20:27:59 +0100

    the getMatchingHeldWeatherData() function now accepts a quarter argument, and uses this as part of it's search criteria

--- src/components/SearchBar/weatherDataRetrieval/getMatchingHeldWeatherData.js
+++ src/components/SearchBar/weatherDataRetrieval/getMatchingHeldWeatherData.js
@@ -1,10 +1,10 @@
-export default function getMatchingHeldWeatherData(weatherData, year) //exported for tests
+export default function getMatchingHeldWeatherData(weatherData, year, quarter = 1) //exported for tests
 {
     let heldMatchingWeatherData = [];
     
     for(let i = 0; i < weatherData.length; i++)
     {
-        if(weatherData[i].year === year)
+        if(weatherData[i].year === year && weatherData[i].quarter === quarter)
         {
             heldMatchingWeatherData.push(weatherData[i]);
         }
--- src/components/SearchBar/weatherDataRetrieval/getMatchingHeldWeatherData.test.js
+++ src/components/SearchBar/weatherDataRetrieval/getMatchingHeldWeatherData.test.js
@@ -2,17 +2,19 @@ import getMatchingHeldWeatherData from './getMatchingHeldWeatherData';
 
 test("getMatchingHeldWeatherData will return any already held data that matches the search criteria", () => {
     const weatherData = [
-        { year: "2020", circuitId: "100" },
-        { year: "2020", circuitId: "200" },
-        { year: "2021", circuitId: "100" }
+        { year: "2020", quarter: 1, circuitId: "100" },
+		{ year: "2020", quarter: 1, circuitId: "200" },
+        { year: "2020", quarter: 2, circuitId: "200" },
+		{ year: "2021", quarter: 1, circuitId: "100" },
+        { year: "2021", quarter: 2, circuitId: "100" }
     ];
     
     //Get all tracks for 2020 ----------------------------------------
     const expectedWeatherData = [
-        { year: "2020", circuitId: "100" },
-        { year: "2020", circuitId: "200" },
+        { year: "2020", circuitId: "100", quarter: 1 },
+        { year: "2020", circuitId: "200", quarter: 1 },
     ];
     
-    const result = getMatchingHeldWeatherData(weatherData, "2020");
+    const result = getMatchingHeldWeatherData(weatherData, "2020", 1);
     expect(result).toStrictEqual(expectedWeatherData);
 });
