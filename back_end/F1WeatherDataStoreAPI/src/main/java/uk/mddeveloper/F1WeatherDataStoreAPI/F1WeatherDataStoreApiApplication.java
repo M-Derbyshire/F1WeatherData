9d6ffc268c375abd6127a012d89b6526b2e52e56
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-26 13:30:29 +0100

    setup the startup for the java API

---
+++ back_end/F1WeatherDataStoreAPI/src/main/java/uk/mddeveloper/F1WeatherDataStoreAPI/F1WeatherDataStoreApiApplication.java
@@ -0,0 +1,13 @@
+package uk.mddeveloper.F1WeatherDataStoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1WeatherDataStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1WeatherDataStoreApiApplication.class, args);
+	}
+
+}
---
+++ back_end/F1WeatherDataStoreAPI/src/test/java/uk/mddeveloper/F1WeatherDataStoreAPI/F1WeatherDataStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.mddeveloper.F1WeatherDataStoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1WeatherDataStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
