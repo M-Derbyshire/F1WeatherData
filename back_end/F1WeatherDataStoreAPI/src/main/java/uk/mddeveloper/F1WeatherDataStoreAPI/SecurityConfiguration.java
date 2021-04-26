70ed37721d3a6d00c1a893aba68f2a066c5f16fb
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-26 20:41:44 +0100

    setup a temp config to allow all routes through spring security

---
+++ back_end/F1WeatherDataStoreAPI/src/main/java/uk/mddeveloper/F1WeatherDataStoreAPI/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package uk.mddeveloper.F1WeatherDataStoreAPI;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.authorizeRequests().antMatchers("/*").permitAll();
+	}
+	
+}
