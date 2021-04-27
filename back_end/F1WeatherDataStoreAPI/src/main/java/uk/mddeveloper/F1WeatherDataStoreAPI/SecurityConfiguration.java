128c20f3c603eeaa7fc376cfb60458ea6cb898d5
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-27 18:55:23 +0100

    the security config now only allows un-authed requests to the year/quarter results route

--- back_end/F1WeatherDataStoreAPI/src/main/java/uk/mddeveloper/F1WeatherDataStoreAPI/SecurityConfiguration.java
+++ back_end/F1WeatherDataStoreAPI/src/main/java/uk/mddeveloper/F1WeatherDataStoreAPI/SecurityConfiguration.java
@@ -13,7 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		http.authorizeRequests().antMatchers("/*").permitAll();
+		http.authorizeRequests().antMatchers("/api/v1/*/*").permitAll();
 		http.cors().and().csrf().disable();
 	}
 	
