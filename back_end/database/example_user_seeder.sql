7708e15bec7791e8cd59f950db817f35a83cc681
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-28 18:40:56 +0100

    wrote a simple sql script to see the Contributer table with an example user

---
+++ back_end/database/example_user_seeder.sql
@@ -0,0 +1,4 @@
+-- The password hash here is hashed with BCrypt.
+-- The password is "Password123"
+INSERT INTO Contributer (email, `password`) VALUES 
+('example@example.com', '$2a$12$.0OJRFc.QFTHY1F1CsnL3utrIrrgjnq.dzSDUb1CFmFsD4gQAIMcO');
