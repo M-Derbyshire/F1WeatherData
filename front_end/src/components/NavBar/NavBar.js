3d4dfc9610afb753693e237d89c4a42b37a7d9bf
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-29 18:35:30 +0100

    added the login link to the navbar

--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 
-function NavBar()
+
+function NavBar(props)
 {
 	return (
 		<nav className="NavBar">
 			<ul>
 				<li><Link to="/">Search for Weather Data</Link></li>
 				<li><Link to="/about">About this App</Link></li>
+				<li><Link to="/login">Login</Link></li>
 			</ul>
 		</nav>
 	);
