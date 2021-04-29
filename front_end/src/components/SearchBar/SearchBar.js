3e9afc512a27ebc570eed0096be3d43d13b80be9
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-04-29 18:59:42 +0100

    moved the data retrieval functions out of the searchbar's folder. These are general purpose

--- front_end/src/components/SearchBar/SearchBar.js
+++ front_end/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SearchBar.css';
-import retrieveWeatherData from './weatherDataRetrieval/weatherDataRetrieval';
+import retrieveWeatherData from '../../dataRetrieval/weatherDataRetrieval';
 
 // Props: 
 // weatherDataState - array of the state and set-state-function for the weatherData
